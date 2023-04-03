d6f35ba6b58c0e37ccb36f213115b0a0987b1120
ObiTracks <straightup.stack@gmail.com>
2023-04-03 00:55:16 -0400

    Got Q1 working in a jupyter notebook. Needs to be tested manually on local as a .py file

---
+++ Assignment 3/q1/predict_sentiment.py
@@ -0,0 +1,38 @@
+import argparse
+import re
+import string
+import sys
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import joblib
+
+def preprocess(text):
+    text = text.lower()
+    text = re.sub(f"[{string.punctuation}]", "", text)
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words("english"))
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+    return " ".join(filtered_tokens)
+
+def main():
+    parser = argparse.ArgumentParser(description="Predict the sentiment of a given text.")
+    parser.add_argument("text", type=str, help="The text to classify.")
+    args = parser.parse_args()
+
+    # Step 1: Preprocess input text
+    text = preprocess(args.text)
+
+    # Step 2: Load the saved model
+    try:
+        model = joblib.load("model.joblib")
+    except FileNotFoundError:
+        print("Error: Model file not found. Please run training_sentiment.py first to train and save the model.")
+        sys.exit(1)
+
+    # Step 3: Predict and print the label
+    prediction = model.predict([text])[0]
+    sentiment = "positive" if prediction == 1 else "negative"
+    print(f"The predicted sentiment of the text is {sentiment}.")
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment 3/q1/training_sentiment.py
@@ -0,0 +1,119 @@
+import argparse
+import os
+import re
+import string
+import numpy as np
+import pandas as pd
+
+from nltk.corpus import stopwords
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+import sklearn.metrics as metrics
+# from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, plot_confusion_matrix
+from sklearn.model_selection import cross_val_predict
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import make_pipeline
+import matplotlib.pyplot as plt
+import joblib
+
+def load_data(file):
+    print("Loading data...")
+    data = []
+    with open("/sentiment_labelled_sentences/" + file, "r", encoding="utf-8") as f:
+        for line in f:
+            text, label = line.strip().split("\t")
+            data.append((text, int(label)))
+    print("Data loaded...")
+    return pd.DataFrame(data, columns=["text", "label"])
+
+def preprocess(text):
+    text = text.lower()
+    text = re.sub(f"[{string.punctuation}]", "", text)
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words("english"))
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+    return " ".join(filtered_tokens)
+
+def train_and_evaluate(args, X, y):
+    if args.naive:
+        clf = MultinomialNB()
+    elif args.knn:
+        clf = KNeighborsClassifier(n_neighbors=args.knn)
+    elif args.svm:
+        clf = SVC()
+    elif args.decisiontree:
+        clf = DecisionTreeClassifier()
+
+    model = make_pipeline(TfidfVectorizer(), clf)
+    y_pred = cross_val_predict(model, X, y, cv=5)
+    
+    print(f"Accuracy: {accuracy_score(y, y_pred)}")
+    print(f"Precision: {precision_score(y, y_pred)}")
+    print(f"Recall: {recall_score(y, y_pred)}")
+    print(f"F-Measure: {f1_score(y, y_pred)}")
+    
+    cm = confusion_matrix(y, y_pred)
+    disp = plot_confusion_matrix(clf, X, y, cmap=plt.cm.Blues, values_format=".0f")
+    plt.show()
+
+    model.fit(X, y)
+    joblib.dump(model, "model.joblib")
+
+def main():
+    parser = argparse.ArgumentParser(description="Train a sentiment analysis model.")
+    parser.add_argument("--imdb", action="store_true", help="Use the IMDB dataset.")
+    parser.add_argument("--amazon", action="store_true", help="Use the Amazon dataset.")
+    parser.add_argument("--yelp", action="store_true", help="Use the Amazon dataset.")
+    parser.add_argument("--naive", action="store_true", help="Use the Naive Bayes classifier.")
+    parser.add_argument("--knn", type=int, help="Use the k-NN classifier.")
+    parser.add_argument("--svm", action="store_true", help="Use the SVM classifier.")
+    parser.add_argument("--decisiontree", action="store_true", help="Use the Decision Tree classifier.")
+    args = parser.parse_args()
+
+    # Step 1: Load data
+
+    train_data = None;
+    test_data = None;
+
+    if args.imdb:
+        train_data = load_data("imdb_labelled.txt")
+        test_data = load_data("imdb_labelled.txt")
+    elif args.amazon:
+        train_data = load_data("amazon_cells_labelled.txt")
+        test_data = load_data("amazon_cells_labelled.txt")
+    elif args.yelp:
+        train_data = load_data("yelp_labelled.txt")
+        test_data = load_data("yelp_labelled.txt")
+    else:
+        print("Please specify either --imdb, --amazon or --yelp.")
+        return
+
+    # Step 2: Preprocess data
+    print("Preprocessing data...")
+    train_data["text"] = train_data["text"].apply(preprocess)
+    test_data["text"] = test_data["text"].apply(preprocess)
+
+    # Step 3: Train and evaluate model
+    print("Training and evaluating model...")
+    X_train, y_train = train_data["text"], train_data["label"]
+    X_test, y_test = test_data["text"], test_data["label"]
+    
+    if args.naive:
+        print("Using Naive Bayes classifier.")
+    elif args.knn:
+        print(f"Using k-NN classifier with k={args.knn}.")
+    elif args.svm:
+        print("Using SVM classifier.")
+    elif args.decisiontree:
+        print("Using Decision Tree classifier.")
+    else:
+        print("Please specify a classifier.")
+        return
+    train_and_evaluate(args, X_train, y_train)
+
